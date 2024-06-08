7867413f3b7561378116ac9077fe24031bc9299c
Marek Kubowicz <marek@kubowicz.pl>
2024-06-08 14:39:08 +0200

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+
+    connection, address = server_socket.accept()    # wait for client
+    print(f"Accept {connection=}, {address=}")
+    connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
 
 
 if __name__ == "__main__":
