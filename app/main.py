55af71071dd4af7255e8c1bb1b45c935bf4e119f
Marek Kubowicz <marek@kubowicz.pl>
2024-06-09 21:08:54 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,29 +1,25 @@
+
 import socket
+from concurrent.futures import ThreadPoolExecutor
+
 from app.routes import app_name
 from app.dispatcher import Dispatcher
 from app.request import Request
 
 
 
-
-
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+def request_processor(server_socket):
     while True:
         connection, address = server_socket.accept()  # wait for client
         print(f"Accept {connection=}, {address=}")
         request = Request(connection)
         Dispatcher.dispatch(request)
-        # if request.path == "/":
-        #     connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
-        # elif request.path.startswith("/echo/"):
-        #     headers = ''
-        #     echo_str = request.path.replace("/echo/", "")
-        #     response = 'HTTP/1.1 200 OK\r\n' + headers + echo_str
-        #     connection.makefile('w').write(response)
-        #
-        # else:
-        #     connection.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
+
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    print(f"Starting server {app_name}")
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        request_futures = [executor.submit(request_processor, server_socket) for _ in range(10)]
 
 
 if __name__ == "__main__":
