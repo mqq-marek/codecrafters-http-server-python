16b4e8a6404aa1d309c0f4c0bd785367b8e93655
Marek Kubowicz <marek@kubowicz.pl>
2024-06-16 21:31:07 +0200

    codecrafters submit [skip ci]

--- app/response.py
+++ app/response.py
@@ -25,20 +25,13 @@ class Response:
         if isinstance(self.body, str):
             self.body = self.body.encode("utf-8")
         if self.request.accept_encoding and "gzip" in self.request.accept_encoding:
-            print("gzip")
             self.headers["Content-Encoding"] = "gzip"
             self.body = gzip.compress(self.body)
-            print(f"gzip {self.body.hex()}")
-        if isinstance(self.body, str):
-            self.body = self.body.encode("utf-8")
         self.headers["Content-Length"] = str(len(self.body))
         header_lines = "\r\n".join(
             key + ": " + value for key, value in self.headers.items()
         )
-        print(self.response_line + header_lines)
-        print(self.body)
-        output = (self.response_line + header_lines + "\r\n\r\n").encode('ascii') + self.body
-        print(output)
+        output = (self.response_line + header_lines + "\r\n\r\n").encode('Latin-1') + self.body
         self.connection.sendall(output)
 
 
